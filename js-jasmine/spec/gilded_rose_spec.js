e05131c47b4ca2d55a3424d0caf5cf5333a11c2a
ikramiste <ikrame.saadi@gmail.com>
2024-02-27 11:48:48 +0200

    brie gets better with time

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -36,6 +36,12 @@ it("An item's quality is never negative", function() {
 "Aged Brie" augmente en qualité au fil du temps.
 */
 
+it("Aged Brie increases in quality the older it gets", function() {
+  const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+  const items = gildedRose.updateQuality();
+  expect(items[0].quality).toEqual(1);
+});
+
 
 /* 4
 La qualité d'un article n'est jamais supérieure à 50, sauf "Sulfuras".
