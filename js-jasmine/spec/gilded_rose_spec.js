35a5ad690064f68e101a39cfbeb745459e006c23
ikramiste <ikrame.saadi@gmail.com>
2024-02-28 14:39:04 +0200

    restored foo

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -1,5 +1,12 @@
 let { Shop, Item } = require("../src/gilded_rose.js");
 describe("Gilded Rose", function () {
+
+  it("should foo", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toEqual("fixme");
+  });
+  
   /* 0
 La qualité baisse de 1 pour les articles standards.
 */
